6b83c023f685db455af69ae0190b027af77399c6
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-09-20 14:25:49 +0300

    Server static files implementation.

--- Cat-Shelter/handlers/home.js
+++ Cat-Shelter/handlers/home.js
@@ -1,8 +1,8 @@
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-//const cats = require('../data/cats');
-//const breeds = require('../data/breeds');
+const cats = require('../data/cats');
+const breeds = require('../data/breeds');
 
 module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
---
+++ Cat-Shelter/handlers/static-files.js
@@ -0,0 +1,47 @@
+const url = require('url');
+const fs = require('fs');
+
+function getContentType(currentUrl) {
+    if (currentUrl.endsWith('css')) {
+        return 'text/css';
+    } else if (currentUrl.endsWith('js')) {
+        return 'application/javascript';
+    } else if (currentUrl.endsWith('png')) {
+        return 'image/png';
+    } else if (currentUrl.endsWith('html')) {
+        return 'text/html';
+    } else if (currentUrl.endsWith('ico')) {
+        return 'image/png';
+    }
+}
+
+module.exports = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname.startsWith('/content') && req.method === 'GET') {
+        fs.readFile(`.${pathname}`, 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.write('Error was found');
+                res.end();
+                return;
+            }
+
+            console.log(pathname);
+            console.log(data);
+            res.writeHead(200, {
+                'Content-Type': getContentType(pathname)
+            });
+
+            res.write(data);
+            res.end;
+        });
+    } else {
+        return true;
+    }
+};
