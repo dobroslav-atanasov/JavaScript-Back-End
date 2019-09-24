5362ed9e1af02e037aa3e2039dbc8b47923a81d7
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-09-24 21:25:26 +0300

    Modified addBreed.html. Implemented post method for adding breed.

--- Cat-Shelter/handlers/cat.js
+++ Cat-Shelter/handlers/cat.js
@@ -2,7 +2,7 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
-// const formidable = require('formidable');
+const formidable = require('formidable');
 const breeds = require('../data/breeds');
 const cats = require('../data/cats');
 
@@ -15,7 +15,10 @@ module.exports = (req, res) => {
         const index = fs.createReadStream(filePath);
 
         index.on('data', (data) => {
-            res.write(data);
+            let catBreedPlaceholder = breeds.map((breed) => `<option value="${breed}">${breed}</option>`)
+            let modifiedData = data.toString().replace('{{catBreeds}}', catBreedPlaceholder);
+
+            res.write(modifiedData);
         });
 
         index.on('end', () => {
@@ -43,9 +46,40 @@ module.exports = (req, res) => {
             console.log(err);
         });
     } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
+        let form = new formidable.IncomingForm();
 
+        form.parse(req, (err, fields, files) => {
+
+        });
+        
     } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
+        let formData = '';
 
+        req.on('data', (data) => {
+            formData += data;
+        });
+
+        req.on('end', () => {
+            let body = qs.parse(formData);
+
+            fs.readFile('./data/breeds.json', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+
+                let breeds = JSON.parse(data);
+                breeds.push(body.breed);
+                let json = JSON.stringify(breeds);
+
+                fs.writeFile('./data/breeds.json', json, 'utf-8', () => {
+                    console.log('The breed was added successfully!');
+                });
+            });
+
+            res.writeHead(200, { location: '/' });
+            res.end();
+        });
     } else {
         return true;
     }
