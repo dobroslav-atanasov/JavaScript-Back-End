268865848253b897afa96446406b3d517db4a799
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-09-25 19:30:36 +0300

    Modified static-files.js. Added pictures.

--- Cat-Shelter/handlers/static-files.js
+++ Cat-Shelter/handlers/static-files.js
@@ -12,6 +12,8 @@ function getContentType(currentUrl) {
         return 'text/html';
     } else if (currentUrl.endsWith('ico')) {
         return 'image/png';
+    } else if (currentUrl.endsWith('jpg')) {
+        return 'image/png';
     }
 }
 
@@ -19,27 +21,51 @@ module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     if (pathname.startsWith('/content') && req.method === 'GET') {
-        fs.readFile(`.${pathname}`, 'utf-8', (err, data) => {
-            if (err) {
-                console.log(err);
-
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
+        if (pathname.endsWith('png') || pathname.endsWith('jpeg') || pathname.endsWith('jpg') && req.method === 'GET') {
+            fs.readFile(`./${pathname}`, (err, data) => {
+                if (err) {
+                    console.log(err);
+    
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+    
+                    res.write('Error was found');
+                    res.end();
+                    return;
+                }
+    
+                console.log(pathname);
+                res.writeHead(200, {
+                    'Content-Type': getContentType(pathname)
                 });
-
-                res.write('Error was found');
+    
+                res.write(data);
                 res.end();
-                return;
-            }
-
-            console.log(pathname);
-            res.writeHead(200, {
-                'Content-Type': getContentType(pathname)
             });
-
-            res.write(data);
-            res.end();
-        });
+        } else {
+            fs.readFile(`.${pathname}`, 'utf-8', (err, data) => {
+                if (err) {
+                    console.log(err);
+    
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+    
+                    res.write('Error was found');
+                    res.end();
+                    return;
+                }
+    
+                console.log(pathname);
+                res.writeHead(200, {
+                    'Content-Type': getContentType(pathname)
+                });
+    
+                res.write(data);
+                res.end();
+            });
+        }
     } else {
         return true;
     }
