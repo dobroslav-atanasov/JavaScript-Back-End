dc74d774422f8a83a6b526dca908feb5b451a78e
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-09-29 15:49:12 +0300

    Implemented details page.

--- Cubicle/config/routes.js
+++ Cubicle/config/routes.js
@@ -3,6 +3,7 @@ const cubeController = require('../controllers/cubeController');
 
 module.exports = (app) => {
     // TODO...
+    app.get('/details/:id', cubeController.details);
     app.get('/create', cubeController.create);
     app.get('/about', cubeController.about);
     app.get('/', cubeController.index);
--- Cubicle/controllers/cubeController.js
+++ Cubicle/controllers/cubeController.js
@@ -10,8 +10,23 @@ function create(req, res) {
     res.render('create.hbs');
 }
 
+function details(req, res) {
+    let id = req.params.id;
+
+    const cube = {
+        id: 1,
+        name: 'cube1',
+        description: 'cube 1 description',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41IpdljUeKL.jpg',
+        difficultyLevel: 1
+    }
+
+    res.render('details.hbs', cube);
+}
+
 module.exports = {
     index,
     about,
-    create
+    create, 
+    details
 };
