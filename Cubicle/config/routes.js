cf4ccb7ed2fb45b2a1b5e8dbc875e8db8c953211
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-09-29 15:34:54 +0300

    Implemented about and create pages.

--- Cubicle/config/routes.js
+++ Cubicle/config/routes.js
@@ -3,5 +3,7 @@ const cubeController = require('../controllers/cubeController');
 
 module.exports = (app) => {
     // TODO...
+    app.get('/create', cubeController.create);
+    app.get('/about', cubeController.about);
     app.get('/', cubeController.index);
 };
--- Cubicle/controllers/cubeController.js
+++ Cubicle/controllers/cubeController.js
@@ -2,6 +2,16 @@ function index(req, res) {
     res.render('index.hbs');
 }
 
+function about(req, res) {
+    res.render('about.hbs');
+}
+
+function create(req, res) {
+    res.render('create.hbs');
+}
+
 module.exports = {
-    index
+    index,
+    about,
+    create
 };
