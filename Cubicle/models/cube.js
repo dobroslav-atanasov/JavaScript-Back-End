2b21c6530d2ca0a7803dbc363880ac0af3346a74
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-09-30 21:37:28 +0300

    Modified model cube.js

--- Cubicle/models/cube.js
+++ Cubicle/models/cube.js
@@ -1,10 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
 class Cube {
     constructor() {
-        this.data = require('../config/database');
+        this.data = require(path.resolve('config/database.json'));
+    }
+
+    getCubeById(id) {
+        const cube = this.data.cubes.find(x => x.id === id);
+        
+        return cube;
     }
 
     add(cubeModel) {
-        
+        const currentIndex = this.data.index + 1;
+        const newCube = { id: currentIndex, ...cubeModel };
+
+        return this.saveChanges(currentIndex, newCube);
+    }
+
+    saveChanges(currentIndex, newCube) {
+        console.log(this.data.cubes);
+        const db = {
+            index: currentIndex,
+            cubes: this.data.cubes.concat(newCube)
+        };
+
+        return this.writeInFile(db, newCube);
+    }
+
+    writeInFile(db, newCube) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path.resolve('config/database.json'), JSON.stringify(db, null, 2), (err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                this.data = db;
+                resolve(newCube);
+            });
+        });
     }
 
     create(name, imageUrl, description, difficultyLevel) {
