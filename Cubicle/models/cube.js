5dc590f5a8b565a61fd9e1de083cfa2f26ad5818
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-10-05 21:01:02 +0300

    Changed cube.js to use mongoose.

--- Cubicle/models/cube.js
+++ Cubicle/models/cube.js
@@ -1,56 +1,29 @@
-const fs = require('fs');
-const path = require('path');
+const mongoose = require('mongoose');
 
-class Cube {
-    constructor() {
-        this.data = require(path.resolve('config/database.json'));
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 255
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 10
+    },
+    accessories: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Accessories'
     }
+});
 
-    getAllCubes() {
-        const cubes = this.data.cubes;
-
-        return cubes;
-    }
-
-    getCubeById(id) {
-        const cube = this.data.cubes.find(x => x.id === id);
-        
-        return cube;
-    }
-
-    add(cubeModel) {
-        const currentIndex = this.data.index + 1;
-        const newCube = { id: currentIndex, ...cubeModel };
-
-        return this.saveChanges(currentIndex, newCube);
-    }
-
-    saveChanges(currentIndex, newCube) {
-        const db = {
-            index: currentIndex,
-            cubes: this.data.cubes.concat(newCube)
-        };
-
-        return this.writeInFile(db, newCube);
-    }
-
-    writeInFile(db, newCube) {
-        return new Promise((resolve, reject) => {
-            fs.writeFile(path.resolve('config/database.json'), JSON.stringify(db, null, 2), (err) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                this.data = db;
-                resolve(newCube);
-            });
-        });
-    }
-
-    create(name, imageUrl, description, difficultyLevel) {
-        return { name, imageUrl, description, difficultyLevel };
-    }
-}
-
-module.exports = new Cube();
+module.exports = mongoose.model('Cube', cubeSchema);
