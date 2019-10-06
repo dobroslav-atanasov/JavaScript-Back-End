7409ec0852df5dec05c529d5736e2693bff78b60
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-10-06 11:28:56 +0300

    Implemented post create method for accessory.

--- Cubicle/controllers/accessoryController.js
+++ Cubicle/controllers/accessoryController.js
@@ -1,7 +1,22 @@
+const accessorySchema = require('../models/accessory');
+
 function getCreateAccessory(req, res) {
     res.render('createAccessory.hbs');
 }
 
+function postCreateAccessory(req, res) {
+    const {name, imageUrl, description} = req.body;
+
+    accessorySchema.create({name, imageUrl, description})
+        .then(accessory => {
+            console.log(accessory);
+            res.redirect('/');
+        }).catch(err => {
+            console.log(err);
+        });
+}
+
 module.exports = {
-    getCreateAccessory
+    getCreateAccessory,
+    postCreateAccessory
 }
