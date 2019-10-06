246eb0e6390fab535aeed81b1a29c233023c5cd9
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-10-06 11:29:16 +0300

    Added new routes.

--- Cubicle/config/routes.js
+++ Cubicle/config/routes.js
@@ -4,6 +4,7 @@ const accessoryController = require('../controllers/accessoryController');
 module.exports = (app) => {
     app.get('/details/:id', cubeController.details);
     app.get('/create/accessory', accessoryController.getCreateAccessory);
+    app.post('/create/accessory', accessoryController.postCreateAccessory);
     app.get('/create', cubeController.getCreateCube);
     app.post('/create', cubeController.postCreateCube);
     app.get('/about', cubeController.about);
