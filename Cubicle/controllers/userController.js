c94673002a46105e17bc24d506a0d813bbb9ed01
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-10-12 22:07:14 +0300

    Implemented register method in userController.js

--- Cubicle/controllers/userController.js
+++ Cubicle/controllers/userController.js
@@ -1,21 +1,41 @@
+const userSchema = require('../models/user');
+
 function getRegister(req, res) {
-    res.render('registerPage.hbs');
+    res.render('register.hbs');
 }
 
 function postRegister(req, res) {
-    
+    const { username, password, repeatPassword } = req.body;
+    if (password !== repeatPassword) {
+        res.render('register.hbs', {
+            errors: {
+                repeatPassword: 'Passwords don\'t match!'
+            }
+        });
+        return;
+    }
+
+    userSchema.create({ username, password }).then(() => {
+        res.redirect('/login');
+    }).catch(err => {
+        res.render('register.hbs', {
+            errors: {
+                username: 'Username already exist!'
+            }
+        });
+    });
 }
 
 function getLogin(req, res) {
-    res.render('loginPage.hbs');
+    res.render('login.hbs');
 }
 
 function postLogin(req, res) {
-    
+
 }
 
 function logout(req, res) {
-    
+
 }
 
 module.exports = {
