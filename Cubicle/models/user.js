8ead5661c958a53d28101a6edd066349ced010ec
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-10-13 18:34:25 +0300

    Added method to user model to compare hash password.

--- Cubicle/models/user.js
+++ Cubicle/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -11,5 +12,11 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.methods = {
+    matchPassword: function (password) {
+        return bcrypt.compare(password, this.hashPassword);
+    }
+};
 
 module.exports = mongoose.model('User', userSchema);
