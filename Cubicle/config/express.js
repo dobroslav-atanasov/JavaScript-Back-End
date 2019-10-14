f0cdefc39a35cbe5ba46a99f3be9136cc7fa852b
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-10-14 18:03:04 +0300

    Added middlewear for cookie-parser.

--- Cubicle/config/express.js
+++ Cubicle/config/express.js
@@ -2,15 +2,19 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
-    //TODO: Setup the view engine
+    // Setup cookie parser
+    app.use(cookieParser());
+
+    // Setup the view engine
     app.engine('.hbs', handlebars({ extname: '.hbs', defaultLayout: false }));
 
-    //TODO: Setup the body parser
+    // Setup the body parser
     app.use(bodyParser.urlencoded({ extended: false }));
 
-    //TODO: Setup the static files
+    // Setup the static files
     app.use(express.static(path.resolve(__basedir, 'static')));
 
     // Set views
