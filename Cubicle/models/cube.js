dff948f4085874ba3b5c055e6d47b6f91c144dca
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-10-19 14:49:47 +0300

    Added validation in model - cube.

--- Cubicle/models/cube.js
+++ Cubicle/models/cube.js
@@ -3,16 +3,42 @@ const mongoose = require('mongoose');
 const cubeSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: [5, 'Cube name should be at least 5 symbols long!'],
+        validate: [
+            {
+                validator: (v) => {
+                    return /[^[a-zA-Z0-9\s]+$]/;
+                },
+                message: props => `${props.value} is not valid name!`
+            }
+        ]
     },
     description: {
         type: String,
         required: true,
-        maxlength: 255
+        minlength: [20, 'Description should be at least 20 symbols long!'],        
+        maxlength: 255,
+        validate: [
+            {
+                validator: (v) => {
+                    return /[^[a-zA-Z0-9\s]+$]/;
+                },
+                message: props => `Description containts invalid symbols!`
+            }
+        ]
     },
     imageUrl: {
         type: String,
-        required: true
+        required: true,
+        validate: [
+            {
+                validator: (v) => {
+                    return v.startsWith('http://') || v.startsWith('https://');
+                },
+                message: props => `Image url should be start with http or https!`
+            }
+        ]
     },
     difficultyLevel: {
         type: Number,
