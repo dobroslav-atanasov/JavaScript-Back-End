705c6d42607f8e60923371276c30a4d70f307c34
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-10-24 09:46:48 +0300

    Removed exam - Project manager.

--- Exam-10.02.2019-Project_Manager/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    development: {
-        port: process.env.PORT || 3000,
-        connectionString: 'mongodb://localhost:27017/project-manager'
-    },
-    production: {}
-};
