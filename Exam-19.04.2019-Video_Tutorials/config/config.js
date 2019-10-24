c65dd691db6d680e452f25538666cfb058bd8030
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-10-24 09:54:26 +0300

    Removed solution of exam Video Tutorials.

--- Exam-19.04.2019-Video_Tutorials/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    development: {
-        port: process.env.PORT || 3333,
-        connectionString: 'mongodb://localhost:27017/video-tutorials'
-    },
-    production: {}
-};
